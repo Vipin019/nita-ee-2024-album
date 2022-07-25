3ec6935051d34db886e69e2630410d5e27e25da5
Vipin019 <vkpmail019@gmail0com>
2022-07-25 05:35:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Main from './Components/Container/Main'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Upload from '../src/Components/Upload/Upload';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <BrowserRouter>
+    <Routes>
+      <Route exact path="/upload" element={<Upload />} />
+      <Route exact path="/" element={<Main />} />
+    </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
