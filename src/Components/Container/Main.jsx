70b4849d66b74f89ce770df6cc3de9188184ac6e
VIPIN KUMAR PATEL <vkpmail019@gmail.com>
2022-12-04 15:22:44 +0530

    alert added for backend

--- src/Components/Container/Main.jsx
+++ src/Components/Container/Main.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react'
 import './Main.css';
 import Player from './Player';
 
-
+alert("https://nita-ee2024-album-backend.onrender.com is not responding please wait.");
 
 const Main = () => {
 
@@ -27,7 +27,7 @@ const Main = () => {
       datas.map(data =>
         <div id="main__images__image">
         <img src={"https://drive.google.com/uc?export=view&id="+data.link.substring(32,65)} alt="image" />
-        <small>Contributed by {data.enrollmentNo}_{data.fullName} on {data.date}</small>
+//         <small>Contributed by {data.enrollmentNo}_{data.fullName} on {data.date}</small>
         </div>
       )}
         
